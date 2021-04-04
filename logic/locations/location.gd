472fbccc4de84085d0d65dd01e8a0d4f467a3737
João Batista <j.batistareis@gmail.com>
2021-04-04 11:33:43 +0100

    improved room transition logic

--- logic/locations/location.gd
+++ logic/locations/location.gd
@@ -32,27 +32,20 @@ func exit(character : Character, newLocationName : String, toSpawnId : int) -> v
 
 
 func move(character : Character, direction : int) -> void:
-	var canPass = false
 	var fromRoom = findRoom(character.currentRoom)
 	var portalId = fromRoom.getPortal(direction)
 	var exitPoint = fromRoom.getExitRoom(direction)
 	
-	if portalId == 0: # no portal, can pass
-		canPass = true
-	else: # see if can pass
-		canPass = findPortal(portalId).canPass(character)
-	
-	if canPass:
-		if exitPoint != 0: # its a room
-			var toRoom = findRoom(exitPoint)
-			fromRoom.exit(character)
-			toRoom.enter(character)
-			Signals.emit_signal("playerChangedRoom", direction)
-			
-		else: # its a location
-			if character.type == Enums.CharacterType.PC:
-				var portal = findPortal(portalId)
-				exit(character, portal.newLocationName, portal.toSpawnId)
+	if (exitPoint > 0) && ((portalId == 0) || findPortal(portalId).canPass(character)): # move to a room
+		var toRoom = findRoom(exitPoint)
+		fromRoom.exit(character)
+		toRoom.enter(character)
+		Signals.emit_signal("playerChangedRoom", direction)
+		
+	elif (character.type == Enums.CharacterType.PC) && (exitPoint == 0) && (portalId > 0): # see if can move to a location
+		if findPortal(portalId).canPass(character):
+			var portal = findPortal(portalId)
+			exit(character, portal.newLocationName, portal.toSpawnId)
 
 
 func executeScript(script : String, character : Character) -> void:
@@ -60,13 +53,31 @@ func executeScript(script : String, character : Character) -> void:
 
 
 func findRoom(id : int) -> Room:
-	return rooms[rooms.bsearch_custom(id, EntityArrayHelper, 'idFind')]
+	var index = rooms.bsearch_custom(id, EntityArrayHelper, 'idFind')
+	
+	if index >= rooms.size():
+		return null
+	
+	var found = rooms[index]
+	return found if (found.id == id) else null
 
 
-func findPortal(id : int) -> Room:
-	return portals[portals.bsearch_custom(id, EntityArrayHelper, 'idFind')]
+func findPortal(id : int) -> Portal:
+	var index = portals.bsearch_custom(id, EntityArrayHelper, 'idFind')
+	
+	if index >= portals.size():
+		return null
+	
+	var found = portals[index]
+	return found if (found.id == id) else null
 
 
-func findSpawn(id : int) -> Room:
-	return spawns[spawns.bsearch_custom(id, EntityArrayHelper, 'idFind')]
+func findSpawn(id : int) -> Spawn:
+	var index = spawns.bsearch_custom(id, EntityArrayHelper, 'idFind')
+	
+	if index >= spawns.size():
+		return null
+	
+	var found = spawns[index]
+	return found if (found.id == id) else null
 
