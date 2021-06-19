f46ff8cc650f45054b6beb0c8fda479c75864bcc
João Batista <j.batistareis@gmail.com>
2021-06-19 16:06:08 +0100

    added exp for next level

--- visuals/gui/windows/statusWindow.gd
+++ visuals/gui/windows/statusWindow.gd
@@ -1,14 +1,14 @@
 class_name StatusWindow
 extends GuiWindowModel
 
-const TEXT : String = '  -[|%s|]-  >HP|%d/%d>LV|%d>Spare pts.|%d>  -| |-| |-| |-| |-  >Strength|%d>Dexterity|%d>Constitution|%d>Intelligence|%d>Wisdom|%d>Charisma|%d'
+const TEXT : String = ' |-[|%s|]-| >HP|%d/%d>LV|%d|[next: %d/%d]>Spare pts.|%d>  -| |-| |-| |-| |-  >Strength|%d>Dexterity|%d>Constitution|%d>Intelligence|%d>Wisdom|%d>Charisma|%d'
 
 
 func _init() -> void:
 	var player = LocationManager.player if (LocationManager.player != null) else Character.new()
 	
 	type = Enums.GuiWindowType.BACKGROUND
-	text = GuiTextModel.new(TEXT % [player.name, player.currentHp, player.maxHp, player.currentLevel, player.sparePoints, player.strength.score, player.dexterity.score, player.constitution.score, player.intelligence.score, player.wisdom.score, player.charisma.score])
+	text = GuiTextModel.new(TEXT % [player.name, player.currentHp, player.maxHp, player.currentLevel, player.experiencePoints, player.experienceToNextLevel, player.sparePoints, player.strength.score, player.dexterity.score, player.constitution.score, player.intelligence.score, player.wisdom.score, player.charisma.score])
 	
 	position = Vector2(
 		OverlayManager.windowSize().x * 0.05,
