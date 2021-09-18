7b40ebbc82ba20022c40bb4ad50f08ad558a283a
João Batista <j.batistareis@gmail.com>
2021-09-18 13:25:35 +0100

    timer window

---
+++ visuals/gui/windows/battleTimerWindow.gd
@@ -0,0 +1,13 @@
+class_name BattleTimerWindow
+extends GuiWindow
+
+
+func _init(players : Array, enemies : Array) -> void:
+	type = Enums.GuiWindowType.FOREGROUND
+	
+	for player in players:
+		widgets.append(GuiCommandProgressWidget.new(player))
+	
+	for enemy in enemies:
+		widgets.append(GuiCommandProgressWidget.new(enemy))
+
