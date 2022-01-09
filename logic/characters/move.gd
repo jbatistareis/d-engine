506afe7aa745f058b9dd846a1b820ca586c81430
João Batista <j.batistareis@gmail.com>
2022-01-09 13:39:56 +0000

    moves now have a pick logic, and do nothing/wait

--- logic/characters/move.gd
+++ logic/characters/move.gd
@@ -2,15 +2,17 @@ class_name Move
 
 const _BASE_VALUE : String = 'func getValue(character : Character) -> int:\n\treturn -character.strength.score'
 const _BASE_OUTCOME : String = 'func getOutcome(character : Character) -> int:\n\treturn Enums.DiceOutcome.BEST'
+const _BASE_PICK : String = 'func pick(character : Character) -> void:\n\tpass'
 
-var name : String = 'Punch'
-var description : String = 'A weak punch'
+var name : String = 'Speed jab'
+var description : String = 'Attacks keeping balance'
 
 var valueExpression : String = _BASE_VALUE
 var outcomeExpression : String = _BASE_OUTCOME
+var pickExpression : String = _BASE_PICK
 
-var cdPre : int = 5
-var cdPost : int = 5
+var cdPre : int = 4
+var cdPost : int = 3
 
 var executions : int = 1
 var persistent : bool = false
@@ -26,4 +28,8 @@ func getResult(character) -> MoveResult:
 	var reference = ScriptTool.getReference(valueExpression + '\n' + outcomeExpression)
 	
 	return MoveResult.new(reference.getValue(character), reference.getOutcome(character))
+
+
+func pick(character) -> void:
+	ScriptTool.getReference(pickExpression).pick(character)
 
