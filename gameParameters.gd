20c8c302ddfd8b2048d8a8a1d5ced7d8587ee113
João Batista <j.batistareis@gmail.com>
2022-05-30 17:59:13 +0100

    mesh -> model, added portal logic to room, renamed logic properties

--- gameParameters.gd
+++ gameParameters.gd
@@ -12,8 +12,8 @@ const MOVE_BASE_EXECUTE : String = 'func execute(_character : Character) -> void
 const FACT_BASE : String = 'func execute(_executor : Character, _suspects : Array) -> Array:\n\treturn CharacterQuery.findByHighestHp(suspects)'
 
 const LOCATION_NOOP : String = 'func execute(_character : Character) -> void:\n\treturn'
-const PORTAL_NOOP : String = 'func execute(_character : Character) -> bool:\n\treturn true'
-const ROOM_TILE_NOOP : String = 'func execute(_character : Character) -> void:\n\treturn'
+const ROOM_ENTER_NOOP : String = 'func execute(_character : Character) -> bool:\n\treturn true'
+const ROOM_NOOP : String = 'func execute(_character : Character) -> void:\n\treturn'
 
 
 var actionBase : Dictionary = {
@@ -27,25 +27,18 @@ var roomBase : Dictionary = {
 	'y': 0,
 	'type': Enums.RoomType._4_EXITS, # use  Enums.RoomType
 	'orientation': Enums.Direction.NORTH , # use Enums.Direction
-	'mesh': '4_exits',
+	'model': '4_exits',
 	'exits': [0, 0, 0, 0, 0, 0],
-	'portals': [0, 0, 0, 0, 0, 0],
-	'entranceLogic': ROOM_TILE_NOOP,
-	'exitLogic': ROOM_TILE_NOOP,
-	'friendlyShortNames': [], # short names of NPCs present in the room
+	'canEnterLogic': ROOM_ENTER_NOOP,
+	'enteringLogic': ROOM_NOOP,
+	'exitingLogic': ROOM_NOOP,
 	'foeShortNameGroups': [], # 2D array representing possible enemy groups
 	'visited': false,
 }
 
-var portalBase : Dictionary = {
-	'id': 1,
-	'passLogic': PORTAL_NOOP,
-	'newLocationShortName': '',
-	'toSpawnId': 2
-}
 
 var spwanBase : Dictionary = {
-	'id': 2,
+	'id': 0,
 	'direction': Enums.Direction.NORTH,
 	'toRoomId': 0
 }
