acc086273cecd91c587ae13121c7b23d3c1435df
João Batista <j.batistareis@gmail.com>
2022-07-17 14:09:57 +0100

    removed unnecessary function

--- tools/location/parameters.gd
+++ tools/location/parameters.gd
@@ -5,13 +5,9 @@ func _ready() -> void:
 	set_tab_disabled(1, true)
 
 
-func goToRoomTab() -> void:
-	set_tab_disabled(1, false)
-	current_tab = 1
-
-
 func _on_grid_selectedMultiRoom(rooms : Array):
 	if !rooms.empty():
 		if !rooms[0].empty():
-			goToRoomTab()
+			set_tab_disabled(1, false)
+			current_tab = 1
 
