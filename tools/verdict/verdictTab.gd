ffcdf183c58764dd7e672ae5c83f21bb64907c35
João Batista <j.batistareis@gmail.com>
2022-08-07 11:13:54 +0100

    verdict tab

---
+++ tools/verdict/verdictTab.gd
@@ -0,0 +1,14 @@
+extends Tabs
+
+
+
+func _on_fileList_item_selected(index):
+	pass # Replace with function body.
+
+
+func _on_txtName_text_changed(new_text):
+	pass # Replace with function body.
+
+
+func _on_txtShortName_text_changed(new_text):
+	pass # Replace with function body.
