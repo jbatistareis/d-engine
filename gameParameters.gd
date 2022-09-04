c676873a63b293234914443cb3c56d04e005a380
João Batista <j.batistareis@gmail.com>
2022-09-04 11:42:15 +0100

    new minimal, wait times

--- gameParameters.gd
+++ gameParameters.gd
@@ -1,7 +1,8 @@
 extends Node
 
 const GCD : float = 0.1
-const WAIT_TICKS : int = 30
+const MIN_CD : int = int(0.5 / GCD)
+const WAIT_TICKS : int = int(3.0 / GCD)
 
 const MOVEMENT_DURATION : float = 0.35
 const ROTATION_DURATION : float = 0.25
