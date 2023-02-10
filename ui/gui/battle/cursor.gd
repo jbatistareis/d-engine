4677301d51f8b1131df711763abadad2ed2aa73a
jbatistareis <j.batistareis@gmail.com>
2023-02-10 14:38:04 +0000

    cursor refactor

--- ui/gui/battle/cursor.gd
+++ ui/gui/battle/cursor.gd
@@ -4,8 +4,8 @@ var enemyFrameSize : Vector2
 
 
 func _ready() -> void:
-	Signals.connect("battleCursorMove",Callable(self,"moveCursor"))
-	Signals.connect("battleCursorHide",Callable(self,"hideCursor"))
+	Signals.battleCursorMove.connect(moveCursor)
+	Signals.battleCursorHide.connect(hideCursor)
 
 
 func moveCursor(title : String, position : Vector2) -> void:
