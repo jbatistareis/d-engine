f39750be7866a56c920e857ebc0c5b467cbc4ccf
jbatistareis <j.batistareis@gmail.com>
2023-02-18 10:50:31 +0000

    base structure

--- tools/armor/armorTab.gd
+++ tools/armor/armorTab.gd
@@ -1,90 +1,9 @@
-extends TabBar
-
-var armorDto : ArmorDTO = ArmorDTO.new()
-var list : Array = []
+extends HBoxContainer
 
 
 func _ready() -> void:
-	EditorSignals.connect("selectedArmor",Callable(self,"loadArmor"))
-	
-	loadAllData()
-	setFields()
+	pass
 
 
-func loadArmor(shortName : String) -> void:
-	armorDto = Persistence.loadDTO(shortName, Enums.EntityType.ARMOR)
-	setFields()
-	
-	$background/mainSeparator/fileList.select(list.find(shortName))
-
-func setFields() -> void:
-	$background/mainSeparator/dataPanel/dataContainer/identification/fields/grid/txtName.text = armorDto.name
-	$background/mainSeparator/dataPanel/dataContainer/identification/fields/grid/txtShortName.text = armorDto.shortName
-	
-	$background/mainSeparator/dataPanel/dataContainer/scaling/fields/grid/sbPositive.value = armorDto.positiveScale
-	$background/mainSeparator/dataPanel/dataContainer/scaling/fields/grid/sbNegative.value = armorDto.negativeScale
-	
-	$background/mainSeparator/dataPanel/dataContainer/integrity/fields/grid/sbCurrIntegrity.value = armorDto.currentIntegrity
-	$background/mainSeparator/dataPanel/dataContainer/integrity/fields/grid/sbMaxIntegrity.value = armorDto.maxIntegrity
-
-
-func loadAllData() -> void:
-	list = Persistence.listEntities(Enums.EntityType.ARMOR)
-	
-	$background/mainSeparator/fileList.clear()
-	for armor in list:
-		$background/mainSeparator/fileList.add_item(armor)
-
-
-
-# files list
-func _on_fileList_item_selected(index):
-	loadArmor($background/mainSeparator/fileList.get_item_text(index))
-
-
-
-# indentification
-func _on_txtName_text_changed(new_text):
-	armorDto.name = new_text
-
-
-func _on_txtShortName_text_changed(new_text):
-	armorDto.shortName = new_text
-
-
-# scaling
-func _on_sbPositive_value_changed(value):
-	armorDto.positiveScale = value
-
-
-func _on_sbNegative_value_changed(value):
-	armorDto.negativeScale = value
-
-
-# integrity
-func _on_sbCurrIntegrity_value_changed(value):
-	armorDto.currentIntegrity = value
-	$background/mainSeparator/dataPanel/dataContainer/integrity/fields/grid/sbMaxIntegrity.min_value = value
-
-
-func _on_sbMaxIntegrity_value_changed(value):
-	armorDto.maxIntegrity = value
-	$background/mainSeparator/dataPanel/dataContainer/integrity/fields/grid/sbCurrIntegrity.max_value = value
-
-
-
-# buttons
-func _on_btnSave_pressed():
-	$saveConfirm.entityName = armorDto.shortName
-	$saveConfirm.popup_centered()
-
-
-func _on_saveConfirm_confirmed():
-	Persistence.saveDTO(armorDto)
-	_on_btnReload_pressed()
-
-
-func _on_btnReload_pressed():
-	loadAllData()
-	$background/mainSeparator/fileList.select(list.find(armorDto.shortName))
-
+func _process(delta: float) -> void:
+	pass
--- tools/editor.gd
+++ tools/editor.gd
@@ -1,21 +1,10 @@
-extends TabContainer
+extends Panel
 
 
 func _ready() -> void:
-	EditorSignals.connect("selectedArmor",Callable(self,"changeTab").bind(0))
-	EditorSignals.connect("selectedInventory",Callable(self,"changeTab").bind(3))
-	EditorSignals.connect("selectedItem",Callable(self,"changeTab").bind(4))
-	EditorSignals.connect("selectedMove",Callable(self,"changeTab").bind(7))
-	EditorSignals.connect("selectedVerdict",Callable(self,"changeTab").bind(8))
-	EditorSignals.connect("selectedWeapon",Callable(self,"changeTab").bind(9))
-	
-	GameManager.testing = true
+	pass
 
 
-func _exit_tree() -> void:
-	GameManager.testing = false
-
-
-func changeTab(ignore, index : int) -> void:
-	current_tab = index
+func _process(delta: float) -> void:
+	pass
 
