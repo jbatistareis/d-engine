daf10cf778077c52278b000d44350734fd289ef3
jbatistareis <j.batistareis@gmail.com>
2023-02-26 13:14:44 +0000

    values passed through a callable, instead of an array

--- tools/fields/optField.gd
+++ tools/fields/optField.gd
@@ -1,40 +1,47 @@
 extends OptionButton
 
 @export
-var parameter : String
+var property : String
 @export_enum("Integer", "String")
 var dataType : int = 0
+var valuesFunc : Callable = Callable(func(): return [])
+var _itemList : Array
+
 
 var dto : DTO :
 	set(value):
 		dto = value
 		
-		if (dataType == 0):
-			select(dto[parameter])
-		elif !dto[parameter].is_empty():
-			select(itemList.find(dto[parameter]))
+		clear()
+		_itemList = valuesFunc.call()
+		for item in _itemList:
+			add_item(item)
+		
+		if dto == null:
+			select(-1)
 		else:
-			select(0)
+			match dataType:
+				0:
+					select(dto[property])
+				
+				1:
+					var index = _itemList.find(dto[property]) 
+					select(index if (index > -1) else 0)
 		
 		disabled = (dto == null)
 
-var itemList : Array :
-	set(value):
-		itemList.clear()
-		clear()
-		
-		itemList = value
-		for item in itemList:
-			add_item(item)
-
 
 func _ready() -> void:
-	item_selected.connect(func(index): dto[parameter] = _getDtoPropertyValue(index))
+	item_selected.connect(func(index): dto[property] = _getDtoPropertyValue(index))
 
 
 func _getDtoPropertyValue(index : int):
-	if (dataType == 0):
-		return index
-	else:
-		return "" if (get_item_text(index) == "None") else itemList[itemList.find(index)]
+	match dataType:
+		0:
+			return index
+		
+		1:
+			return "" if (get_item_text(index) == "None") else _itemList[_itemList.find(index)]
+	
+	return null
 
